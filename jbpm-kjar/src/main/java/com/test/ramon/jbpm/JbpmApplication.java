5c0117290291e3ac335267d71025dfa30f1298f6
Albareda, Ramon <ramon.albereda@sky.uk>
2026-03-24 22:13:00 +0100

    add spring

---
+++ jbpm-kjar/src/main/java/com/test/ramon/jbpm/JbpmApplication.java
@@ -0,0 +1,11 @@
+package com.test.ramon.jbpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbpmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JbpmApplication.class, args);
+    }
+}
---
+++ jbpm-kjar/src/main/java/com/test/ramon/jbpm/controller/ProcessController.java
@@ -0,0 +1,24 @@
+package com.test.ramon.jbpm.controller;
+
+import org.kie.api.runtime.KieSession;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/process")
+public class ProcessController {
+
+    private final KieSession kieSession;
+
+    public ProcessController(KieSession kieSession) {
+        this.kieSession = kieSession;
+    }
+
+    @PostMapping("/start")
+    public String startProcess() {
+        System.out.println("Starting jBPM process...");
+        kieSession.startProcess("demo.process");
+        return "Process demo.process started!";
+    }
+}
